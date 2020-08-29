4706a233283b792f974cb388cd844a2908399ffd
Tolani-kitan <omotolanimodupe@gmail.com>
2020-08-29 23:38:30 +0100

    still working on AddRoom

--- client/src/context/bookings/BookingState.js
+++ client/src/context/bookings/BookingState.js
@@ -10,7 +10,6 @@ const BookingState = (props) => {
       {
         room: "Oceania Meeting Room",
         userName: "Christiana Adebayo",
-        shared: "3 people",
         date: "28/08/2020",
         image:
           "//res.cloudinary.com/nnamdy/image/upload/v1592992885/Pub_Picture_13_oaqmna.png",
@@ -20,32 +19,29 @@ const BookingState = (props) => {
       {
         room: "Macey's Lobby",
         userName: "Maggie Crowe",
-        shared: "17 people",
         date: "15/08/2020",
         image:
           "https://res.cloudinary.com/nnamdy/image/upload/v1592992816/Pub_Picture_7_mrv4a0.png",
         time: "12:00PM - 01:00PM",
-        status: "Cancelled",
+        status: "Upcoming",
       },
       {
         room: "Atlantic Room",
         userName: "Andrews Peter",
-        shared: "2 people",
         date: "03/09/2020",
         image:
           "https://res.cloudinary.com/nnamdy/image/upload/v1592992870/Pub_Picture_12_mtkily.png",
         time: "10:00AM - 11:00AM",
-        status: "Completed",
+        status: "Upcoming",
       },
       {
         room: "Hi-Fi Conference Room",
         userName: "James Brown",
-        shared: "23 people",
         date: "30/08/2020",
         image:
           "https://res.cloudinary.com/nnamdy/image/upload/v1592992857/Pub_Picture_11_qdilnw.png",
         time: "08:00AM - 10:00AM",
-        status: "Completed",
+        status: "Upcoming",
       },
     ],
     filtered: null,
